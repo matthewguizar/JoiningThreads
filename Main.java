c61a2cc7e87624688ae0da98c734a474fd993591
Matthew Guizar <matthewrguizar@gmail.com>
2022-08-23 14:02:45 -0700

    future task

--- Main.java
+++ Main.java
@@ -1,17 +1,35 @@
 import java.util.Scanner;
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
 
 public class Main {
 
-    static final double TARGET = 0.5;
-    static final double PRECISION = 0.000000001;
+    
+    static double[] targets = new double [] {0.5, 0.8, 0.3};
+    static final double PRECISION = 0.0000001;
     static double result = 0;
 
     public static void main(String[] args) {
 
         //call generateNumber here...
-        Runnable runnable = () -> result = generateNumber();
-        Thread thread2 = new Thread(runnable);
+        // Runnable runnable = () -> result = generateNumber();
+        //Callable<Double> callable = () -> generateNumber(); //if your task returns something use callable
+        //threads aren't designed to return results can only be created out of a runnable
+        //future task implements Runnable interface
+        FutureTask<Double> future = new FutureTask<>(() -> generateNumber(0));
+        //so future task can behave as a runnable
+        Thread thread2 = new Thread(future);
+
+        FutureTask<Double> future2 = new FutureTask<>(() -> generateNumber(1));
+        Thread thread3 = new Thread(future);
+
+        FutureTask<Double> future3 = new FutureTask<>(() -> generateNumber(2));
+        Thread thread4 = new Thread(future);
+
+
         thread2.start();
+        thread3.start();
+        thread4.start();
         //calculate precision level here...
         
 
@@ -19,14 +37,16 @@ public class Main {
         System.out.println("Please enter a name to generate a number: ");
         scan.nextLine();
         scan.close();
-        double precision = difference(result);
+        // double precision = difference(result);
         try {
-            thread2.join();
+            future.get();
+            future2.get();
+            future3.get();
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        System.out.println("The computer returned a value of: " + result);
-        System.out.println("The value was generated to a precision of : " + precision);
+        System.out.println("finished running background operations");
+        // System.out.println("The value was generated to a precision of : " + precision);
     }
 
     /**
@@ -37,19 +57,19 @@ public class Main {
      *   1. Generates a number close to the TARGET to a precision of PRECISION.
      * 
      */
-    public static double generateNumber(){
+    public static double generateNumber(int index){
         double number = Math.random();
-        double difference = difference(number);
+        double difference = difference(number, index);
         while (difference > PRECISION) {
             number = Math.random();
-            difference = difference(number);
+            difference = difference(number, index);
         }
         return number;
 
     }
 
-    public static double difference(double number) {
-        return Math.abs(TARGET - number);
+    public static double difference(double number, int index) {
+        return Math.abs(targets[index] - number);
     }
 
 }
